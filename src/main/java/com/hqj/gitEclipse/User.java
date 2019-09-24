980535de2f5ae9b8c814b23e56e4a6e74e40e25f
hqj111 <741105430@qq.com>
2019-09-25 02:10:34 +0800

    hot_fix by git1

--- src/main/java/com/hqj/gitEclipse/User.java
+++ src/main/java/com/hqj/gitEclipse/User.java
@@ -2,4 +2,17 @@ package com.hqj.gitEclipse;
 
 public class User {
 	private String right;
+	private int id;
+	public String getRight() {
+		return right;
+	}
+	public void setRight(String right) {
+		this.right = right;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
