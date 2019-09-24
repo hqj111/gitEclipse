53f33642ea5450651a20f249ee161150b8eadb8a
hqj111 <741105430@qq.com>
2019-09-25 01:43:33 +0800

    modify User.java right

--- src/main/java/com/hqj/gitEclipse/User.java
+++ src/main/java/com/hqj/gitEclipse/User.java
@@ -1,9 +1,5 @@
 package com.hqj.gitEclipse;
 
 public class User {
-<<<<<<< HEAD
 	private String right;
-=======
-	private String left;
->>>>>>> branch 'master' of https://github.com/hqj111/gitEclipse.git
 }
